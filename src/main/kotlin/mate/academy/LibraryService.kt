f8c0e96b53cc35fef5ec7deb64519af7ec875421
mc704ed <mykyta.churylov@student.tuke.sk>
2026-04-01 23:04:37 +0200

    ff

--- src/main/kotlin/mate/academy/LibraryService.kt
+++ src/main/kotlin/mate/academy/LibraryService.kt
@@ -1,20 +1,28 @@
 package mate.academy
 
 class LibraryService {
+    val library: MutableList<Book> = mutableListOf()
     fun addBook(book: Book) {
-        // TODO: implement
+        library.add(book)
     }
 
     fun searchByTitle(title: String): List<Book> {
-        // TODO: implement
+        if (title == "")
+            return library
+
+        return library.filter { it.title.contains(title, true) }
     }
 
 
     fun searchByAuthor(authorName: String): List<Book> {
-        // TODO: implement
+       return library.filter { book -> book.authors.any { it.name.contains(authorName, true) } }
     }
 
     fun searchByGenre(genre: String): List<Book> {
-        // TODO: implement
+        return library.filter { it.genre.equals(genre, ignoreCase = true) }
     }
 }
+
+data class Author(val name: String)
+data class Book(val title: String, val authors: List<Author>, val publicationYear: Int,
+                val genre: String, val isbn: String)
